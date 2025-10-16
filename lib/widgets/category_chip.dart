9e6dd4f1446876e49eae6b9224fe0276d0647886
Nisrina Asad <nisrina@nisrina-mekbuk.local>
2025-10-16 08:57:41 +0700

    creating category_chip widget

---
+++ lib/widgets/category_chip.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:tribun_app/utils/app_colors.dart';
+
+class CategoryChip extends StatelessWidget {
+  final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+
+  const CategoryChip({super.key, required this.label, required this.isSelected, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 8),
+      child: FilterChip(
+        label: Text(label),
+        selected: isSelected,
+        onSelected: (_) => onTap(),
+        backgroundColor: Colors.grey[100],
+        selectedColor: AppColors.primary.withValues(alpha: 0.2),
+        checkmarkColor: AppColors.primary,
+        labelStyle: TextStyle(
+          color: isSelected ? AppColors.primary : AppColors.textSecondary,
+          fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+        ),
+
+      ),
+
+    );
+  }
+}
