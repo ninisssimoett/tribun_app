e3a5dbd3f42f29c1833dfce2424fb29628f59906
Nisrina Asad <nisrina@nisrina-mekbuk.local>
2025-10-20 09:27:32 +0700

    defining an app routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: constant_identifier_names
+
+part of 'app_pages.dart';
+
+
+abstract class Routes {
+  Routes._();
+
+  static const SPLASH = _Paths.SPLASH;
+    static const NEWS_DETAIL = _Paths.NEWS_DETAIL;
+
+}
+
+//pendeklerasian route dari masing masing screen
+abstract class _Paths {
+  _Paths._();
+  static const SPLASH = '/splash';
+  static const HOME = '/home';
+  static const NEWS_DETAIL = '/news-detal';
+}
