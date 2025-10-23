8f82fdcf22a65e8573e9af6b82942f5ed074622f
Nisrina Asad <nisrina@nisrina-mekbuk.local>
2025-10-23 10:52:36 +0700

    compliting splash screen adn home screen

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -7,7 +7,8 @@ abstract class Routes {
   Routes._();
 
   static const SPLASH = _Paths.SPLASH;
-    static const NEWS_DETAIL = _Paths.NEWS_DETAIL;
+  static const HOME = _Paths.HOME;
+  static const NEWS_DETAIL = _Paths.NEWS_DETAIL;
 
 }
 
